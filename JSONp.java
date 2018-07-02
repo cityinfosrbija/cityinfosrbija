8a8fbfed1aa2b1693b2327e934044ebb9523753b
cityinfosrbija <cityinfosrbija@gmail.com>
2018-07-02 18:02:04 +0200

    Radna verzija

--- JSONp.java
+++
@@ -1,8 +0,0 @@
-package com.mzbcacak.kele.cityinfosrbija;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public interface JSONp {
-    void populate(JSONObject data) throws JSONException;
-}
